9fcaf9e2ee935e9382e28be2b6ba95ee6c1504c4
Helloflix <shboby71@gmail.com>
2024-03-16 21:39:36 +0530

    Rust Trash Codes.rs

---
+++ Books/Rust Trash Codes.rs
@@ -0,0 +1,9 @@
+//scope
+fn main() {
+    let  x: i32 = 100;
+    let  y: i32 = 101;
+    {
+        println!("value x = {}  value y is {}", x, y);
+    }
+     println!("value x = {}  value y is {}", x, y);
+}
