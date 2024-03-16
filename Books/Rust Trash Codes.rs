685763383539bc4d9b2548a01ae15d1b47cac2f9
Helloflix <shboby71@gmail.com>
2024-03-17 00:18:04 +0530

    Rust Trash Codes.rs

--- Books/Rust Trash Codes.rs
+++ Books/Rust Trash Codes.rs
@@ -19,3 +19,30 @@ fn define_Data(){
    let  x : i32 = 100;
     println!("The data is {}", x);
 }
+
+
+fn main(){
+    define_Data();
+}
+
+fn define_Data(){
+   let  x : i32 = 100;
+    println!("The data is {}", x);
+}
+
+// shadow
+fn main(){
+    let data : i32 = 100;
+    let data = "hello, sire";
+    println!("the real data is {}" , data);
+    }
+
+fn main() {
+    let x: i32 = 100;
+    {
+        let x = 10;
+        assert_eq!(x, 10);
+    }
+    assert_eq!(x, 100);
+    let x = 50;
+}
