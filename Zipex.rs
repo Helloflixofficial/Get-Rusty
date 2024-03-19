e8d67261c5366afb472eb7048a2646b7fd278e83
Helloflix <shboby71@gmail.com>
2024-03-19 23:02:41 +0530

    Zipex.rs

--- Zipex.rs
+++ Zipex.rs
@@ -14,4 +14,34 @@ fn real_main() -> i32 {
     let filename = fs::File::open(&fname).unwrap();
     let mut archive = zip.ZipArchive::new(filename).unwrap();
 
-}
+    for i in 0..archive.len()  
+    let output = match file.enclosed_name(){
+        some(path) => path.to_owned();
+        none => continue,
+
+    };
+    {
+        let comment =file.comment();
+        if!(comment).is_empty(){
+            printlm!("filename {} comment {}",i,comment);
+        }
+
+        if(*file.name())ends_with('/'){
+            println!("File {} Extracted to \"{}\"", i, outpath.display()); 
+            fs:: create_dir_all(&outpath).unwrap();
+        }else{
+            println!("File {} Extracted to \"{}\" ({} bytes)",
+            i,
+            outpath.display(),
+            file.size()
+        );
+
+         if let some(p) =outpath.parant(){
+            if !p.exists(){
+                fs:: create_dir_all(&p).unwrap()
+            }
+         }
+
+        }
+    };
+};
