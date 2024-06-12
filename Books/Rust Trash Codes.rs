38760ddabf8a1eabc2f454385dfc71f90c0086b8
Helloflix <shboby71@gmail.com>
2024-06-12 23:20:43 +0530

    Update Rust Trash Codes.rs

--- Books/Rust Trash Codes.rs
+++ Books/Rust Trash Codes.rs
@@ -625,6 +625,40 @@ fn main() {
 
 
         
+pub mod songs {
+    pub fn play(name:String) {
+        println!("track selection: {}", name);
+    } 
+}
+
+use songs::play;
+
+// Exercise Solution
+
+pub mod tracks {
+    pub mod rock {
+        pub mod indie {
+            pub fn select(name:String) {
+                println!("Track selection is: {}", name);
+            }
+        }
+    }
+}
+
+use tracks::rock::indie::select;
+// imported a public module
+
+fn main() {
+//    play("Kissed By A Rose".to_string());
+
+select("Serenade".to_string());
+select("French Baguette".to_string());
+select("Pineapple Blues".to_string());
+
+}
+
+
+        
 
 
 
