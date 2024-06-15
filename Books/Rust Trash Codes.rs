0d883d929cda92e48d6e4e7c7305ddb58c97993c
Helloflix <shboby71@gmail.com>
2024-06-15 23:38:29 +0530

    Update Rust Trash Codes.rs

--- Books/Rust Trash Codes.rs
+++ Books/Rust Trash Codes.rs
@@ -658,6 +658,27 @@ select("Pineapple Blues".to_string());
 }
 
 
+            println!("{} years old", 36);
+    
+    // named arguments also work
+    
+    println!("{user1}{action}{user2}",
+            user1="Mary Lewis",
+            user2="Jon Wick",
+            action=" karate kicked ");
+            
+    
+// Exercise: Within the function main add a println macro which takes two arguments, 
+//x and y and stringifies them where x equals "hello " and 
+// y equals "my friend".
+
+    println!("{x}, {y}",
+    x="hello",
+    y="my friend")
+            
+}
+
+
         
 
 
