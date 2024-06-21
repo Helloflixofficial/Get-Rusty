de1d1b82baf13cd04c102f5978700ae6ccbdb408
Helloflix <shboby71@gmail.com>
2024-06-21 22:38:13 +0530

    Update Hash-Map.rs

--- Hash-Map.rs
+++ Hash-Map.rs
@@ -1,10 +1,19 @@
 // Proper 
 use std::collections::HashMap;
 fn main(){
-     let mut accountInfo = HashMap::new();
-     accountInfo.insert("Boby","Overdraft!");
-     accountInfo.insert("Ravi","Good Standing!");
-     accountInfo.insert("Robby","Insufficient funds!");
-     accountInfo.insert("Raviya","Overpowered!");
-  println!("The size of the map is {}", accountInfo.len());
+     let mut accountinfo = HashMap::new();
+     accountinfo.insert("Boby","Overdraft!");
+     accountinfo.insert("Ravi","Good Standing!");
+     accountinfo.insert("Robby","Insufficient funds!");
+     accountinfo.insert("Raviya","Overpowered!");
+  println!("The size of the map is {}", accountinfo.len());
+  let mut bardrinks =  HashMap:: new();
+  bardrinks.insert("vodka",true);
+  bardrinks.insert("beer",false);
+  bardrinks.insert("whiskey",true);
+  println!("THE size of thebardrinks is {}",bardrinks.len()); //before 
+  bardrinks.remove(&"whiskey");
+  println!("THE size of thebardrinks is {}",bardrinks.len());//after
 }
+
+
