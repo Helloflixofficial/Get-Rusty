c8817b4296526b362827ea21c31478ddb8914a6e
Helloflix <shboby71@gmail.com>
2024-06-28 23:02:04 +0530

    BigONotation.rs

--- Books/BigONotation.rs
+++ Books/BigONotation.rs
@@ -14,3 +14,22 @@ fn main(){
     println!("the sum is {:#}",result);
     println!("the sum is {:#}",result2);
 }
+
+
+
+
+# Array
+nums = [1, 2, 3]
+nums.append(4)    # push to end
+nums.pop()        # pop from end
+nums[0]           # lookup
+nums[1]
+nums[2]
+
+
+# HashMap / Set
+hashMap = {}
+hashMap["key"] = 10     # insert
+print("key" in hashMap) # lookup
+print(hashMap["key"])   # lookup
+hashMap.pop("key")      # remove
