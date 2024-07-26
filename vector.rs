1beea138c1daca5c1bc15066d68559f2c6d30be5
Helloflix <shboby71@gmail.com>
2024-07-26 22:40:06 +0530

    Create vector.rs

---
+++ vector.rs
@@ -0,0 +1,28 @@
+//basic's
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    println!("{:?}", v);
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+    println!("{:?}", v);
+}
+
+fn main() {
+    let mut v = vec![1, 2, 3];
+    v.push(4);
+    println!("{:?}", v);
+}
+
+fn main() {
+    let mut v = vec![1, 2, 3, 4];
+    v.pop(); // removes the last element
+    println!("{:?}", v);
+
+    v.remove(1); // removes the element at index 1
+    println!("{:?}", v);
+}
