a75cd3d353f2a0e02fe2bcbe732f36dca70740ab
Helloflix <shboby71@gmail.com>
2024-08-08 22:38:31 +0530

    Update panic.rs

--- Books/panic.rs
+++ Books/panic.rs
@@ -12,3 +12,27 @@ fn seven(n:i32) -> Result <bool, String> {
         return Err("This is not  a seven".to_string());
     }
 }
+
+
+
+use std::fs::File;
+
+ fn main() {
+ 
+     // panic!("This will cause the program to abruptly end");
+     
+     // let f = File::open("doesnotexist.txt").expect("No such thing!");
+     
+     let solution = is_seven(8).unwrap();
+     println!("the solution is {}", solution);
+     println!("The progam is working here");
+
+ } 
+ 
+ fn is_seven(n:i32) -> Result <bool,String> {
+     if n == 7 {
+        return Ok(true);
+     } else {
+        return Err("This is not seven".to_string());
+     }
+ }
