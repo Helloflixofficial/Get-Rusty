8c2e3f7958c05299ee6a2d16522e008d9ea80b46
Helloflix <shboby71@gmail.com>
2024-08-16 22:01:26 +0530

    Update Linked List topics .rs

--- Books/Linked List topics .rs
+++ Books/Linked List topics .rs
@@ -24,3 +24,26 @@ fn main() {
     let vector = vec![0, 1, 2, 3, 4];
     println!("{:?}", to_list(vector));
 }
+
+
+
+
+
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut new_node = None;
+        let mut original_node = head;
+
+        while let Some(mut node) = original_node {
+            original_node = node.next; // advancing the current node
+            node.next = new_node;
+            new_node = Some(node);
+        }
+        return new_node;
+    }
+}
+
+
+
+
