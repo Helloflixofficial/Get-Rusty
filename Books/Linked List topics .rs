a8e09fa5f56032d6685a137fedbd407cef8af12a
Helloflix <shboby71@gmail.com>
2024-08-21 22:28:31 +0530

    Update Linked List topics .rs

--- Books/Linked List topics .rs
+++ Books/Linked List topics .rs
@@ -89,4 +89,83 @@ mod test {
 
 
 
+#![allow(unused)]
+struct Boeing {
+    required_crew: u8,
+    range: u16,
+}
+struct Airbus {
+    required_crew: u8,
+    range: u16,
+}
+trait Flight {
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool;
+}
+
+impl Flight for Boeing {
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
+        if (available_crew >= required_crew) && (range + 150 > distance) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl Flight for Airbus {
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
+        if (available_crew >= required_crew) && (range + 150 > distance) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn main() {
+    let boeing = Boeing {
+        required_crew: 4,
+        range: 7370,
+    };
+    let airbus = Airbus {
+        required_crew: 7,
+        range: 5280,
+    };
+
+    let boeing_is_legal = boeing.is_legal(boeing.required_crew, 18, boeing.range, 2385);
+
+    let airbus_is_legal = airbus.is_legal(airbus.required_crew, 3, airbus.range, 2200);
+    println!(
+        "is the Boeing flight legal ? {} \n is the Airbus flight is legal? {}",
+        boeing_is_legal, airbus_is_legal
+    );
+}
+
+
+
+
+#![allow(unused)]
+struct Waypoint {
+    name: String,
+    latitude: f64,
+    longitude: f64,
+}
+
+struct Segment {
+    start: Waypoint,
+    end: Waypoint,
+}
+
+fn main() {
+    const EARTH_REDIUS_IN_KILOMETEERS: f64 = 6371.0;
+
+    let mut data = Waypoint {
+        name: "sharmaji".to_string(),
+        latitude: 41.4075,
+        longitude: -81.851111,
+    };
+}
+
+
+
 
