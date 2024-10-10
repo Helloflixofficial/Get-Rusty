3f99c4c46d4b957ed041ff8c594251569a8e019c
Helloflix <shboby71@gmail.com>
2024-10-10 20:04:51 +0530

    Update Two_sum.rs

--- two_sum_dif.rs
+++ two_sum_dif.rs
@@ -1,37 +1,29 @@
-impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.iter().len() {
-        for j in i + 1..nums.iter().len() {
-            if nums[i] + nums[j] == target {
-                return vec![i as i32, j as i32];
-            }
+use std::collections::HashMap;
+pub struct Solution;
+impl Solution{
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::new();
+    
+    for (i, &num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(&index) = map.get(&complement) {
+            return vec![index as i32, i as i32];
         }
+        map.insert(num, i);
     }
     vec![]
 }
-};
+}
 
 #[cfg(test)]
-mod test{
-  use super::*;
-
+mod test {
+    use super::*;
     #[test]
     fn example1() {
-        essert_eq!(two_sum(vec![2,7,11,15],),vec![0,1])
-
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
     }
-
-    #[test] 
-    fn example2() {
-        errert_eq!(two_sum(vec![3,2,4],6), vec![1,2]);
-    }
-
-    #[test] 
-    fn example3() {
-        errert_eq!(two_sum(vec![3,3],6), vec![0,1]);
-    }
+ 
 }
 
 
-/// consider adding a `main` function to `src/main.rs  lol lol lol lol not gonna do it 
 
