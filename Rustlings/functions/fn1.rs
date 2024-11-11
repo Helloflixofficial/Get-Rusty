321ce0baf15614ecda319b46b4dd7e552087548d
Helloflix <shboby71@gmail.com>
2024-11-11 20:39:37 +0530

    Create fn1.rs

---
+++ Rustlings/functions/fn1.rs
@@ -0,0 +1,7 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    println!("YO TO Honey singh");
+}
+fn main() {
+    call_me(); // Don't change this line
+}
