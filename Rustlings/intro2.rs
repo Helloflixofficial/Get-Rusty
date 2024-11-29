7b1be4fca2bf82c6c0566f5d2e5314d7862bbd6b
Helloflix <shboby71@gmail.com>
2024-11-29 21:00:17 +0530

    Update intro2.rs

--- Rustlings/intro2.rs
+++ Rustlings/intro2.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello world!");
 }
+
+
+use std::time::Instant;
+
+fn main() {
+    // Start measuring time
+    let start = Instant::now();
+
+    // Code block to measure
+    let mut sum = 0;
+    for i in 0..1_000_000 {
+        sum += i;
+    }
+
+    // End measuring time
+    let duration = start.elapsed();
+
+    println!("Result: {}", sum);
+    println!("Execution Time: {:?}", duration);
+}
