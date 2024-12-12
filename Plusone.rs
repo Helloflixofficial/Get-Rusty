134506521f64dbc8b9df15361e0e84c45583c3aa
Helloflix <shboby71@gmail.com>
2024-12-12 22:19:36 +0530

    Create Plusone.rs

---
+++ Plusone.rs
@@ -0,0 +1,16 @@
+
+impl Solution {
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        for i in (0..digits.len()).rev() {
+            if digits[i] < 9 {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        let mut result = vec![1];
+        result.extend(digits);
+        result
+    }
+}
