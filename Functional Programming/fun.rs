bce2854003e17559b5752fec611723fa03bb3855
Helloflix <shboby71@gmail.com>
2024-12-18 22:13:48 +0530

    Create fun.rs

---
+++ Functional Programming/fun.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5];
+    let doubled: Vec<i32> = numbers.iter().map(|x| x * 2).collect();
+    println!("Doubled: {:?}", doubled);
+}
+
+
+//More But next day
