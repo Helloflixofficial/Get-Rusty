d897d39e303750e7ad272598478280352119edf0
Helloflix <shboby71@gmail.com>
2024-12-28 22:46:57 +0530

    Update xml & struct.rs

--- xml & struct.rs
+++ xml & struct.rs
@@ -1,13 +1,21 @@
 use serde::{Deserialize, Serialize};
 use serde_xml_rs::{from_str, to_string};
+
 fn main() {
-    let xml = r#"<Person><name>BOBY<name/><age>24<age/><Person/>"#;
+    let xml = r#"<Person><name>BOBY</name><age>24</age></Person>"#;
 
-    let person: Person = from_str(xml).unwrap();
-    println!("{:?}", person);
-
-    let person_data = to_string(&person).unwrap();
-    println!("{}", person_data);
+ 
+    let person: Result<Person, _> = from_str(xml);
+    match person {
+        Ok(person) => {
+            println!("{:?}", person);
+            let person_data = to_string(&person).unwrap();
+            println!("{}", person_data);
+        }
+        Err(e) => {
+            eprintln!("Failed to parse XML: {}", e);
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
