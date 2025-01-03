a8ebe008312b28f1126f80de6c50412c2b87b6e0
Helloflix <shboby71@gmail.com>
2025-01-03 22:54:39 +0530

    Update async.rs

--- async.rs
+++ async.rs
@@ -1,12 +1,17 @@
-// learning 
-
-use futures::executor::block_on;
-
-fn main() {
-    let f = hello();
-    block_on(f);
+use num_bigint::BigUint;
+use tokio::time::{sleep, Duration};
+async fn async_add_big_numbers(a: BigUint, b: BigUint) -> BigUint {
+    sleep(Duration::from_secs(1)).await;
+    a + b
 }
 
-async fn hello() {
-    println!("hello sire");
+#[tokio::main]
+async fn main() {
+    let num1 = BigUint::parse_bytes(b"123456789123456789123456789", 10).unwrap();
+    let num2 = BigUint::parse_bytes(b"987654321987654321987654321", 10).unwrap();
+
+    println!("Adding big numbers...");
+    let result = async_add_big_numbers(num1, num2).await;
+
+    println!("Result: {}", result);
 }
