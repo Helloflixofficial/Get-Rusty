69f53c9c095e536c007bef9c93dc806eb3e33d44
Helloflix <shboby71@gmail.com>
2025-01-08 22:43:46 +0530

    Update deep_syntex-practice.rs

--- deep_syntex-practice.rs
+++ deep_syntex-practice.rs
@@ -27,3 +27,30 @@ fn main() {
     println!("{:?}", other_arr);
 }
 // Tuple
+fn main() {
+    let tuple: (u8, bool, f32) = (5, true, 2.1);
+    let tuple2 = (3, 5);
+    // print structure of array and other objects
+    println!("first {}, second {}, third {}", tuple.0, tuple.1, tuple.2);
+    println!("{:?}", tuple2);
+    let (a, b, c) = tuple;
+    // destructuring
+    println!("first {}, second {}, third {}", a, b, c);
+}
+
+//function
+fn main(){
+    println!("{}", is_even(2))
+}
+
+pub fn is_even(num:u8) -> bool{
+    let digit: u8 = num % 2;
+    digit == 0
+}
+// Mutability
+fn main(){
+    let mut data = 10;
+    let data = 5;
+    println!("{}",data);
+}
+
