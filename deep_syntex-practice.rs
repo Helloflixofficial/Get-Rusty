0d0dbfdaa680767642fb002cb50b4e7529ccc7cf
Helloflix <shboby71@gmail.com>
2025-01-11 22:35:02 +0530

    Update deep_syntex-practice.rs

--- deep_syntex-practice.rs
+++ deep_syntex-practice.rs
@@ -104,3 +104,22 @@ map.remove(&0);
 println!("{:?}",map);
 }
 
+//
+fn main() {
+    let name = String::from("Bird");
+    let bird = Bird { name, attack: 5 };
+    bird.print_name();
+}
+
+struct Bird {
+    name: String,
+    attack: u64,
+}
+
+impl Bird {
+    fn print_name(&self) {
+        println!("{}", self.name);
+    }
+}
+// interface
+
