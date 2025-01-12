5df483f759b1c43f06ebd7407423e84892bc0741
Helloflix <shboby71@gmail.com>
2025-01-12 23:04:43 +0530

    Update deep_syntex-practice.rs

--- deep_syntex-practice.rs
+++ deep_syntex-practice.rs
@@ -123,3 +123,20 @@ impl Bird {
 }
 // interface
 
+
+// Options & Panic
+fn divide(dividend: i32, divisor:i32) -> Option<i32>{
+    if dividend % divisor != 0 {
+        None
+    } else {
+        Some(dividend / divisor)
+    }
+} 
+
+
+fn main(){
+let divide1: Option<i32> = divide(4,2);
+let divide2: Option<i32> = divide(2,3);
+println!("{:?} :unwrap to : {:?}", divide1, divide1.unwrap()); 
+println!("{:?} :unwrap to : {:?}", divide2, divide2.unwrap()); 
+}
