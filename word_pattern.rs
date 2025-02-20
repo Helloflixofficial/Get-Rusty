7e2b05bf7baa73655e2314fb8f823b61da15f7a3
Helloflix <shboby71@gmail.com>
2025-02-20 23:32:53 +0530

    Update word_pattern.rs

--- word_pattern.rs
+++ word_pattern.rs
@@ -11,25 +11,32 @@ impl Solution {
 
         let mut char_to_word = HashMap::with_capacity(pattern.len());
         let mut word_to_char = HashMap::with_capacity(pattern.len());
+        for (c, &word) in pattern.chars().zip(words.iter()) {
+            
+            if let Some(&mapped_word) = char_to_word.get(&c) {
+                if mapped_word != word {
+                    return false;
+                }
+            } else {
+                char_to_word.insert(c, word);
+            }
 
-
-
-
-
-
-
-// practice next day!
-
-
-
-
-
+           
+            if let Some(&mapped_char) = word_to_char.get(word) {
+                if mapped_char != c {
+                    return false;
+                }
+            } else {
+                word_to_char.insert(word, c);
+            }
+        }
+        true
     }
 }
 
 fn main() {
     let pattern = "abba".to_string(); 
-    let s = "dog cat cat dog".to_string();
+    let s = "dog cat cat dog".to_string(); 
     println!("{}", Solution::word_pattern(pattern.clone(), s)); 
 
     let s2 = "dog cat cat fish".to_string(); 
