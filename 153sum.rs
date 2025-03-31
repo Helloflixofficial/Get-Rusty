c7cb9be15b6b04d3d15b9eee93b82752c17f41b1
Helloflix <shboby71@gmail.com>
2025-03-31 22:47:55 +0530

    Create 153sum.rs

---
+++ 153sum.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+struct Solution;
+impl Solution{
+    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+          let mut nums = nums;
+          nums.sort();
+          let mut result = Vec::new()
+
+   for i in 0..nums.len() {
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+   }
+
+//Incomplete Brain is Not braining right now
+
+
+
+    }
+}
+
+fn main(){
+    let  nums = vec![-1, 0, 1, 2, -1, -4];
+    let data = Solution::three_sum(nums);
+    println!("{:?}",data);
+}
