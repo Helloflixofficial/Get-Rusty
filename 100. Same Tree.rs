9fc36c559305e0f90c02b6020b4273d0e354ff79
Helloflix <shboby71@gmail.com>
2025-04-08 22:25:34 +0530

    Create 100. Same Tree.rs

---
+++ 100. Same Tree.rs
@@ -0,0 +1,33 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+struct Solution;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+    }
+}
+fn main() {
+    let mut p = [1,2,3], 
+   let mut  q = [1,2,3]
+}
