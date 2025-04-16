724962a38d8090b15775105ac93ebc756ad99cf7
Helloflix <shboby71@gmail.com>
2025-04-16 21:32:43 +0530

    Create today.rs

---
+++ today.rs
@@ -0,0 +1,43 @@
+struct Solution;
+
+impl Solution{
+    pub fn add_binary(a : String , b : String ) -> {
+        let mut result = String::new();
+        let mut carry = 0;
+        let ba = a.as_bytes();
+        let bb = b.as_bytes();
+
+        let mut i = a.len() as i32 -1;
+        let mut j =b.len() as i32 -1;
+       
+       while i > 0 || j > 0 || carry > 0 {
+        let mut sum  = carry;
+
+
+        if i >= 0 {
+            sum += (ba[i as unsize] - b'0') as u8;
+            i -= 1;
+
+        }
+
+        if i >= 0 {
+            sum += (ba[i as usize] - b'0') as u8;
+            i -= 1;
+        }
+
+
+
+       }
+
+
+    }
+}
+
+
+
+fn main() {
+    let a = "1010".to_string();
+    let b = "1011".to_string();
+    let result = Solution::add_binary(a, b);
+    println!("Binary sum = {}", result);
+}
