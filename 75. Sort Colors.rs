4dead0c7b0ff4de2254fa8348e4214ce8a458edc
Helloflix <shboby71@gmail.com>
2025-04-27 22:33:12 +0530

    Update 75. Sort Colors.rs

--- 75. Sort Colors.rs
+++ 75. Sort Colors.rs
@@ -2,27 +2,27 @@ fn sort_colors(nums: &mut Vec<i32>) {
     let mut low = 0;
     let mut mid = 0;
     let mut high = nums.len() - 1;
-    println!("The data is : {}" , high);
-     while mid <= high {
-          match nums[mid] {
 
-             0 => {
+    while mid <= high {
+        match nums[mid] {
+            0 => {
                 nums.swap(low, mid);
                 low += 1;
                 mid += 1;
             },
-             1 => {
+            1 => {
                 mid += 1;
             },
-
-
-
-
-
-
-          }
-     }
+            2 => {
+                nums.swap(mid, high);
+                if high == 0 { break; } 
+                high -= 1;
+            },
+            _ => panic!("Invalid input, only 0, 1, 2 allowed."),
+        }
+    }
 }
+
 
 
 fn main() {
