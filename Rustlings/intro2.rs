b7b2b3b0254b8ff5c4ff9eaf0d0e4ec430657900
Helloflix <shboby71@gmail.com>
2025-04-29 22:24:28 +0530

    Update intro2.rs

--- Rustlings/intro2.rs
+++ Rustlings/intro2.rs
@@ -21,3 +21,23 @@ fn main() {
     println!("Result: {}", sum);
     println!("Execution Time: {:?}", duration);
 }
+
+
+
+let expr = "12+3-4";
+let mut num = String::new();
+
+for c in expr.chars() {
+    match c {
+        '0'..='9' => num.push(c),
+        '+' | '-' => {
+            println!("Number: {}", num);
+            println!("Operator: {}", c);
+            num.clear();
+        }
+        _ => (),
+    }
+}
+if !num.is_empty() {
+    println!("Number: {}", num);
+}
