016f0371266f75e0fedbfdd13d3860be400ab64b
Helloflix <shboby71@gmail.com>
2025-07-11 23:09:57 +0530

    Create Fruit Into Baskets.rs

---
+++ Fruit Into Baskets.rs
@@ -0,0 +1,25 @@
+// # 904. Fruit Into Baskets
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+//     You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+//     Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+//     Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn total_fruit(fruits: Vec<i32>) -> i32 {
+//Read Carefull Then i can go for this que........
+
+
+
+
+    }
+}
+fn main() {
+    let fruits = vec![1, 2, 1, 2, 3];
+    let result = Solution::total_fruit(fruits);
+    println!("Max fruits collected: {}", result);
+}
