acb40ea68f9ad479aac9cecdeb96b6b5e68fda29
Helloflix <shboby71@gmail.com>
2025-08-17 23:35:18 +0530

    Update practice1.rs

--- LinkedList/leet/practice1.rs
+++ LinkedList/leet/practice1.rs
@@ -1,28 +1,46 @@
-fn main() {
-    let mut ll = LinkedList::new();
-    ll.push_frunt(10);
-    ll.push_back(20);
-    ll.push_frunt(30);
-}
+use std::cell::RefCell;
+use std::rc::Rc;
 
+#[derive(Debug)]
 struct Node {
-    element: u32,
-    next: List,
+    value: i32,
+    next: Option<Rc<RefCell<Node>>>,
 }
 
-enum List {
-    Empty,
-    Link(Box<Node>),
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::*;
-    #[test]
-    fn test1() {
-        let list = List::Link(Box::new(Node {
-            element: 1024,
-            next: List::Empty,
-        }));
+impl Node {
+    fn new(value: i32) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node { value, next: None }))
     }
+}
+
+#[derive(Debug)]
+struct LinkedList {
+    head: Option<Rc<RefCell<Node>>>,
+}
+
+impl LinkedList {
+    fn new() -> Self {
+        LinkedList { head: None }
+    }
+
+    fn push_front(&mut self, value: i32) {
+        let newnode = Node::new(value);
+        newnode.borrow_mut().next = self.head.take();
+        self.head = Some(newnode);
+    }
+
+    fn print(&self) {
+        let mut current = self.head.clone();
+        while let Some(node) = current {
+            print!("{} -> ", node.borrow().value);
+            current = node.borrow().next.clone();
+        }
+        println!("NON");
+    }
+}
+
+fn main() {
+    let mut list = LinkedList::new();
+    list.push_front(10);
+    list.print();
 }
