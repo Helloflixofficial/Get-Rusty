22048b8c5f6d52771050e8cdf873878a8523075b
Helloflix <shboby71@gmail.com>
2025-08-18 22:13:30 +0530

    Create backfront.rs

---
+++ LinkedList/leet/backfront.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct LinkedList<T>(Option<(T, Box<LinkedList<T>>)>);
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList(None)
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        let t = self.0.take();
+        self.0 = Some((data, Box::new(LinkedList(t))));
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        match self.0 {
+            Some((_, ref mut child)) => child.push_back(data),
+            None => self.push_front(data),
+        }
+    }
+}
+
+fn main() {
+    let mut list = LinkedList::new();
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+
+    println!("{:?}", list);
+}
